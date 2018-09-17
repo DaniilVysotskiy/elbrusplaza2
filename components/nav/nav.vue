ae60be204235a91ef40ecaae4c39790590fe823e
Daniil Vysotskiy <daniil.vysotskiy@teligent.ru>
2018-09-17 13:19:12 +0300

    media: add css toggle menu

--- components/nav/nav.vue
+++ components/nav/nav.vue
@@ -5,23 +5,27 @@
         .nav__logo
           a.logo__wrapper(href="/")
             img.logo(:src="changeLogoOnScroll")
-        .nav__menu.menu
-          ul.menu__list
-            li.menu__item.menu-item
-              nuxt-link.menu-item__link.menu-item__link--hotel(to="#hotel" active-class="active") {{ $t('LINKS.HOTEL') }}
-            li.menu__item.menu-item
-              nuxt-link.menu-item__link.menu-item__link--rooms(to="#rooms" active-class="active") {{ $t('LINKS.ROOMS') }}
-            li.menu__item.menu-item
-              nuxt-link.menu-item__link.menu-item__link--reviews(to="#reviews" active-class="active") {{ $t('LINKS.REVIEWS') }}
-            li.menu__item.menu-item
-              nuxt-link.menu-item__link.menu-item__link--video(to="#my-video" active-class="active") {{ $t('LINKS.VIDEO') }}
-            li.menu__item.menu-item
-              nuxt-link.menu-item__link.menu-item__link--contacts(to="#contacts" active-class="active") {{ $t('LINKS.CONTACTS') }}
-          ul.menu__lang
-            li.menu__lang-item
-              nuxt-link.menu__lang-button(to="/ru" active-class="active" @click="setLang('ru')") RU
-            li.menu__lang-item
-              nuxt-link.menu__lang-button(to="/en" active-class="active" @click="setLang('en')") EN
+        .nav__menu.menu#menu
+          .menu__buttons
+            a.menu__button.menu__button--open(href="#menu") ☰
+            a.menu__button.menu__button--close(href="#_") ⨯
+          div.menu-toggle
+            ul.menu__list
+              li.menu__item.menu-item
+                nuxt-link.menu-item__link.menu-item__link--hotel(to="#hotel" active-class="active") {{ $t('LINKS.HOTEL') }}
+              li.menu__item.menu-item
+                nuxt-link.menu-item__link.menu-item__link--rooms(to="#rooms" active-class="active") {{ $t('LINKS.ROOMS') }}
+              li.menu__item.menu-item
+                nuxt-link.menu-item__link.menu-item__link--reviews(to="#reviews" active-class="active") {{ $t('LINKS.REVIEWS') }}
+              li.menu__item.menu-item
+                nuxt-link.menu-item__link.menu-item__link--video(to="#my-video" active-class="active") {{ $t('LINKS.VIDEO') }}
+              li.menu__item.menu-item
+                nuxt-link.menu-item__link.menu-item__link--contacts(to="#contacts" active-class="active") {{ $t('LINKS.CONTACTS') }}
+            ul.menu__lang
+              li.menu__lang-item
+                nuxt-link.menu__lang-button(to="/ru" active-class="active" @click="setLang('ru')") RU
+              li.menu__lang-item
+                nuxt-link.menu__lang-button(to="/en" active-class="active" @click="setLang('en')") EN
 </template>
 
 <script>
@@ -56,6 +60,9 @@ export default {
     },
     setLang(lang) {
       this.$store.commit('SET_LANG', lang);
+    },
+    toggleMenu() {
+      console.log('menu button clicked!');
     }
   },
   computed: {
@@ -66,6 +73,9 @@ export default {
         return smallLogo;
       }
     }
+  },
+  mounted() {
+    this.isTopOfPage = window.scrollY > 1 ? false : true;
   },
   beforeMount () {
     window.addEventListener('scroll', throttle(this.handleScroll));
